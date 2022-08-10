f8c68d6b46205379c9b97a51483d336f9d47a58e
Alexin Danila <danya09kz@gmail.com>
2022-08-10 16:42:32 +0600

    Create template-literals.js

---
+++ template-literals.js
@@ -0,0 +1,18 @@
+const user = 'Bob'
+const num = 17
+
+const txt = `
+Hello ${user}! You are ${num}
+Now is ${Date.now()}
+`
+console.log(txt)
+
+
+const html = `
+<ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+</ul>
+`
+console.log(html)
